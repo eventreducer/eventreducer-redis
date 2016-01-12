e9572dced77d283a0a38fee1443b7b85629c8436
Yurii Rashkovskii <yrashk@gmail.com>
2016-01-11 16:39:31 -0800

    Extract JSON parts into eventreducer-json

--- src/main/java/org/eventreducer/redis/RedisJournal.java
+++ src/main/java/org/eventreducer/redis/RedisJournal.java
@@ -1,27 +1,15 @@
 package org.eventreducer.redis;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.Version;
-import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
-import com.fasterxml.jackson.databind.jsontype.TypeDeserializer;
-import com.fasterxml.jackson.databind.jsontype.TypeSerializer;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
-import org.apache.commons.net.ntp.TimeStamp;
-import org.eventreducer.*;
+import org.eventreducer.Event;
+import org.eventreducer.IndexFactory;
+import org.eventreducer.Journal;
 import org.eventreducer.hlc.PhysicalTimeProvider;
+import org.eventreducer.json.EventReducerModule;
 import org.redisson.RedissonClient;
 import org.redisson.codec.JsonJacksonCodec;
 import org.redisson.core.RMap;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.StreamSupport;
@@ -41,15 +29,7 @@ public class RedisJournal extends Journal {
             @Override
             protected ObjectMapper initObjectMapper() {
                 ObjectMapper objectMapper = super.initObjectMapper();
-                SimpleModule module;
-                module = new SimpleModule("RedisJournal", new Version(1,0,0,null, "org.eventreducer.redis", "RedisJournal"));
-
-                module.addSerializer(new TimestampSerializer());
-                module.addSerializer(new SerializerSerializer());
-                module.addDeserializer(TimeStamp.class, new TimestampDeserializer());
-                module.setMixInAnnotation(Serializable.class, SerializableMixin.class);
-                objectMapper.registerModule(module);
-
+                objectMapper.registerModule(new EventReducerModule());
                 return objectMapper;
             }
         };
@@ -68,64 +48,6 @@ public class RedisJournal extends Journal {
 
     }
 
-    public static abstract class SerializableMixin {
-
-        @JsonIgnore
-        private Serializer serializer;
-
-        @JsonProperty("@hash")
-        public abstract <T extends Serializable> Serializer<T> entitySerializer() throws ClassNotFoundException, IllegalAccessException, InstantiationException;
-
-    }
-
-    static class SerializerSerializer extends StdSerializer<Serializer> {
-        protected SerializerSerializer() {
-            super(Serializer.class);
-        }
-
-        @Override
-        public void serialize(Serializer value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-            gen.writeBinary(value.hash());
-        }
-
-        @Override
-        public void serializeWithType(Serializer value, JsonGenerator gen, SerializerProvider serializers, TypeSerializer typeSer) throws IOException {
-            serialize(value, gen, serializers);
-        }
-    }
-
-    static class TimestampSerializer extends StdSerializer<TimeStamp> {
-
-        protected TimestampSerializer() {
-            super(TimeStamp.class);
-        }
-
-        @Override
-        public void serialize(TimeStamp value, JsonGenerator gen, SerializerProvider provider) throws IOException {
-            gen.writeString(value.toString());
-        }
-
-        @Override
-        public void serializeWithType(TimeStamp value, JsonGenerator gen, SerializerProvider serializers, TypeSerializer typeSer) throws IOException {
-            gen.writeString(value.toString());
-        }
-    }
-
-    static class TimestampDeserializer extends StdDeserializer<TimeStamp> {
-        public TimestampDeserializer() {
-            super(TimeStamp.class);
-        }
-
-        @Override
-        public TimeStamp deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
-            return new TimeStamp(p.readValueAs(String.class));
-        }
-
-        @Override
-        public Object deserializeWithType(JsonParser p, DeserializationContext ctxt, TypeDeserializer typeDeserializer) throws IOException {
-            return new TimeStamp(p.readValueAs(String.class));
-        }
-    }
 
     @Override
     protected void journal(List<Event> events) {
